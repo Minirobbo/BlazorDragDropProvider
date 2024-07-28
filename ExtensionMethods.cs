f243fee957a1bb89d1e87fbb0dcd3e1afccd709e
Isaac Robinson <isaac@irobinson.org>
2024-07-28 15:15:12 +1000

    Added IServiceCollection extension method to add all required services in a single call.

---
+++ ExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragDropProvider
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Checks if another type is the same or is a subclass of current type.
+        /// </summary>
+        /// <param name="self">This Type class</param>
+        /// <param name="other">Type class to be compared to</param>
+        /// <returns>True, if the other Type class is the same or is a subclass of the current Type, otherwise False</returns>
+        public static bool IsSameOrSubClassOf(this Type self, Type other)
+        {
+            return self.IsSubclassOf(other) || self == other;
+        }
+
+        /// <summary>
+        /// Adds required services to a dependancy injection IServiceCollection
+        /// </summary>
+        /// <param name="services">Current services to be added to</param>
+        public static void AddDragDropServices(this IServiceCollection services)
+        {
+            services.AddScoped<DragDropService>();
+        }
+    }
+}
